d29ffef719f49e53d0408ddf545c9992bd5e80ba
Eugene Gordeiev <eugene@macbook.phub.net.cable.rogers.com>
2018-02-07 16:46:40 -0500

    First commit

---
+++ src/view/MyMessageBox.js
@@ -0,0 +1,8 @@
+Ext.define('Test.view.MyMessageBox', {
+    extend : 'Ext.window.Window',
+    alias : 'widget.mymessagebox',
+    
+    modal: 'true',
+    height:Ext.getBody().getViewSize().height*.50,
+    width:Ext.getBody().getViewSize().width*.50
+});
